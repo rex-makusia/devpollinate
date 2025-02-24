6990e7467f88ee874df28988871d343001cfbf15
Grecpt <grecpt@gmail.com>
2025-02-25 05:57:43 +1000

    Install Tailwindcss with related dependencies

---
+++ tailwindcss.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./layouts/**/*.html", "./content/**/*.md", "./themes/**/*.html"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
